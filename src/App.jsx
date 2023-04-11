8f6441fcc04c27e4bd94f93c74c2f62fa1a4d256
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-04-11 22:24:16 +0100

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import search from "./components/search/search";
+import Search from "./components/search/search";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleOnSearchChange = (searchData) => {
+    console.log();
+  };
 
   return (
     <div className="App">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
