ef0b9d3798a60213932c2e5d5ce975171d6651ce
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-04-11 22:24:25 +0100

    added api fetch

---
+++ src/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5992067263msh3ac49f7d11e01c4p1b4c63jsn6a3cb9f50912",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
