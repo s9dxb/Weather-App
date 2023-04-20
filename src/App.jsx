1a3e356c0c06f9762647c8b9eb278464a3b754fd
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-04-20 01:56:07 +0100

    added api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Search from "./components/search/search";
 import "./App.css";
 import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log();
+    const [lat, lon] = searchData.value.split(" ");
+
+    const CurrentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid={API key}`
+    );
   };
+  //i stopped at 47:42
 
   return (
-    <div className="App">
+    <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       <CurrentWeather />
     </div>
