15da33c72eaa79c4883e2ebcee43649c23ff042a
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-04-20 01:12:17 +0100

    added bottom div

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -10,6 +10,32 @@ const CurrentWeather = () => {
         </div>
         <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
